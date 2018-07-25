873e594b714773b07d0bf72104c0f1668f8402e3
Key-F <kirill2010.97@mail.ru>
2018-07-25 11:17:10 +0300

    3rd test passes
    Поиск по тексту в документе

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -94,87 +94,41 @@ public class test {
         MobileElement testel = driver.findElementById("com.consultantplus.app:id/search_results_title"); // Выбор первого результата
         testel.click();
 
-        MobileElement el7 = driver.findElement(By.id("com.consultantplus.app:id/search_edit")); // Поиск строки поиска
-        String Textin = el7.getText();
+        MobileElement searchstring = driver.findElement(By.id("com.consultantplus.app:id/search_edit")); // Поиск строки поиска
+        String Textin = searchstring.getText();
 
         Assert.assertEquals(Textin, prop.getProperty("firstSearch"));
 
     }
 
+    @Test // [UT.3]
     public void searchInTextTest () throws InterruptedException {
 
-        if (isElementPresent(By.id("com.consultantplus.app:id/button_intro_prev")) == true) { // Если есть ли приветствие
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+
+        if (isElementPresent(By.id("com.consultantplus.app:id/button_intro_prev")) == true) { // Если есть приветствие
             MobileElement el1 = (MobileElement) driver.findElementById("com.consultantplus.app:id/button_intro_prev");
             el1.click(); // Пропускаем вступительную часть
         }
 
         wait.until(ExpectedConditions.visibilityOfElementLocated
-                (By.id("com.consultantplus.app:id/search_edit"))).sendKeys(prop.getProperty("firstSearch"));
+                (By.id("com.consultantplus.app:id/search_edit"))).sendKeys(prop.getProperty("firstSearch")); // Ввод первого запроса
         ((AndroidDriver)driver).pressKeyCode(66);
-        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
 
-        // while (isElementPresent(By.id("com.consultantplus.app:id/retry_btn")) == true) {
-        //     driver.findElement(By.id("com.consultantplus.app:id/retry_btn")).click();
-        // }
-        MobileElement testel = driver.findElementById("com.consultantplus.app:id/search_results_title");
-        String NalogText = testel.getText();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("com.consultantplus.app:id/search_results_title")));
+        MobileElement testel = driver.findElementById("com.consultantplus.app:id/search_results_title"); // Выбор первого результата
         testel.click();
 
-        // while (isElementPresent(By.id("com.consultantplus.app:id/retry_btn")) == true) {
-        //    driver.findElement(By.id("\tcom.consultantplus.app:id/retryprogress_view_retry")).click();
-        // }
-        MobileElement Head = driver.findElement(By.xpath("//android.view.View[@content-desc='НАЛОГОВЫЙ']"));
-        MobileElement SecondHead = driver.findElement(By.xpath("//android.view.View[@content-desc='КОДЕКС']"));
-        MobileElement Who = driver.findElement(By.xpath("//android.view.View[@content-desc=' РОССИЙСКОЙ ФЕДЕРАЦИИ']"));
-        assert((Head.getText() + SecondHead.getText() + Who.getText()).toLowerCase() == "налоговый кодекс российской федерации");
-
-        MobileElement el7 = driver.findElement(By.id("com.consultantplus.app:id/search_edit"));
-        String Textin = el7.getText();
-        el7.clear();
-        el7.sendKeys(prop.getProperty("secondSearch"));
+        MobileElement searchstring = driver.findElement(By.id("com.consultantplus.app:id/search_edit")); // Поиск поисковой строки
+        searchstring.clear();
+        searchstring.sendKeys(prop.getProperty("secondSearch")); // Выполнение второго запроса
         ((AndroidDriver)driver).pressKeyCode(66);
-        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-        MobileElement el11 = (MobileElement) driver.findElement(By.xpath("//android.view.View[@content-desc='Статья 27. ']"));
-        boolean eba = el11.isDisplayed();
 
-
-
-        //
-        driver.findElement(By.xpath("//*[@resource-id='com.consultantplus.app:id/list_item_entry_text']"));
-        //wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.support.v4.widget.DrawerLayout/android.view.ViewGroup/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.support.v4.view.ViewPager/android.widget.RelativeLayout/android.widget.ListView/android.widget.RelativeLayout[1]/android.widget.LinearLayout/android.widget.TextView")));
-        //MobileElement el4 = (MobileElement) driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.support.v4.widget.DrawerLayout/android.view.View/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.support.v4.view.ViewPager/android.widget.RelativeLayout/android.widget.ListView/android.widget.RelativeLayout[1]/android.widget.LinearLayout/android.widget.TextView");
-        //el4.click();
-        MobileElement el77 = (MobileElement) driver.findElementByAccessibilityId("Поиск по тексту");
-        el77.click();
-        el77.sendKeys("Законный представитель налогоплательщика");
-        MobileElement el88 = (MobileElement) driver.findElementByAccessibilityId("Поиск по тексту");
-        el88.click();
-        MobileElement el9 = (MobileElement) driver.findElementById("com.consultantplus.app:id/search_edit");
-        el9.sendKeys("Законный представитель налогоплательщика");
-        MobileElement el10 = (MobileElement) driver.findElementById("com.consultantplus.app:id/button_next");
-        el10.click();
-
-
-
-
-
-        //Click and pass Tutorial
-        wait.until(ExpectedConditions.visibilityOfElementLocated
-                (By.id("com.isinolsun.app:id/btn_skip"))).click();
-
-        //Click to "Is Ariyorum" Button
-        wait.until(ExpectedConditions.visibilityOfElementLocated
-                (By.id("com.isinolsun.app:id/bluecollar_type_button"))).click();
-
-
-        //Notification Allow
-        if (driver.findElements(By.id("com.android.packageinstaller:id/permission_allow_button")).size()>0) {
-            driver.findElements(By.id("com.android.packageinstaller:id/permission_allow_button")).get(0).click();
-        }
-
-        //Click second job
-        // wait.until(ExpectedConditions.visibilityOfElementLocated
-        //        (By.xpath(secondNewJob))).click();
+        // Проверка наличия необходимого текста
+        driver.findElement(By.xpath("//android.view.View[@content-desc='Статья 27. ']"));
+        driver.findElement(By.xpath("//android.view.View[@content-desc='Законный']"));
+        driver.findElement(By.xpath("//android.view.View[@content-desc='представитель']"));
+        driver.findElement(By.xpath("(//android.view.View[@content-desc='налогоплательщика'])[1]"));
 
     }
     @AfterMethod
